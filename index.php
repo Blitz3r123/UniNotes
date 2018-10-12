6dcc945cbb630f78e2c897fd57133476d9643d4d
Kaleem <KaleemPeeroo@gmail.com>
2018-10-12 10:34:36 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 		<link rel="stylesheet" href="css/indexStyle.css">
 	</head>
 	<body>
-<!--        hello this is yordan-->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -20,7 +19,114 @@
 				<a href="pdit/index.html" class="list-group-item list-group-item-action">IN2015 Professional Development in IT</a>
 				<a href="cpp/index.html" class="list-group-item list-group-item-action">IN2029 Programming in C++</a>
 			</div>
-		</div>
 
+			<h1>Website Completion Checklist</h1>
+			<ul class="list-group">
+				<li class="list-group-item">
+					Data Structures and Algorithms 
+					<ul class="list-group">
+						<li class="list-group-item">
+						Section 1 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-success">Complete</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 2 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes<span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 3 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-success">Completed</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+					</ul>
+				</li>
+				<li class="list-group-item">
+					Object Oriented Analysis and Design
+					<ul class="list-group">
+						<li class="list-group-item">
+						Section 1 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 2 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes<span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 3 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+					</ul>
+				</li>
+				<li class="list-group-item">
+					Professional Development in IT
+					<ul class="list-group">
+						<li class="list-group-item">
+						Section 1 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 2 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes<span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 3 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+					</ul>
+				</li>
+				<li class="list-group-item">
+					Programming in C++
+					<ul class="list-group">
+						<li class="list-group-item">
+						Section 1 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-success">Complete</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 2 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes<span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+						<li class="list-group-item">
+						Section 3 <span class="badge badge-pill badge-warning">In Progress</span>
+						<ul class="list-group">
+							<li class="list-group-item">Lecture Notes <span class="badge badge-pill badge-warning">In Progress</span></li>
+							<li class="list-group-item">Quiz<span class="badge badge-pill badge-warning">In Progress</span></li>
+						</ul>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	</body>
 </html>
